95651f7cdb6809bc321daf3ba0b4b7f0651d88cf
Ronald Arias <ariasron@hotmail.com>
2022-02-09 20:19:26 -0500

    Use resetKeys to reset the error boundary

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -61,7 +61,7 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => setPokemonName('')}>
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => setPokemonName('')} resetKeys={[pokemonName]}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
